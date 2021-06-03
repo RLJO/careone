66bf9d1369983a424c30a144626d9cbbd4de1c87
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2021-06-03 13:28:20 +0200

    Update account_move.py

--- careone_reports/models/account_move.py
+++ careone_reports/models/account_move.py
@@ -12,3 +12,9 @@ class AccountMove(models.Model):
             for line in item.invoice_line_ids:
                 if line.discount:
                     item.total_discount += line.price_unit * (line.discount / 100)
+
+    def _compute_access_url(self):
+        super(AccountMove, self)._compute_access_url()
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        for move in self.filtered(lambda move: move.is_invoice()):
+            move.access_url = base_url + '/my/invoices/%s' % (move.id)
